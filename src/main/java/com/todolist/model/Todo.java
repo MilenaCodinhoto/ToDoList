0ac7ef0e7cef9ed3d8f25d817c6f13b80da9359f
Milena <milena.cod@gmail.com>
2026-03-06 18:16:04 -0300

    Base do projeto e classe Todo criada

---
+++ src/main/java/com/todolist/model/Todo.java
@@ -0,0 +1,40 @@
+package com.todolist.model;
+
+public class Todo {
+    Long id;
+    String titulo;
+    String descricao;
+    Boolean concluida;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(Boolean concluida) {
+        this.concluida = concluida;
+    }
+}
